32b6ea069f615e066898509818fb66919c51a716
ThallesSorrilha <thallessorrilha@gmail.com>
2025-03-28 17:21:18 -0300

    main2 correction

--- lib/aula_sintaxe/main2.dart
+++ lib/aula_sintaxe/main2.dart
@@ -1,19 +1,25 @@
 import 'package:app1/aula_sintaxe/Carro_callback.dart';
-import 'package:app1/aula_sintaxe/pessoa.dart';
 import 'dart:io';
+
+class Pessoa {
+  String? nome;
+  int? idade;
+
+  Pessoa({this.nome, this.idade});
+}
 
 void main() {
   var pessoa = Pessoa();
   print('Nome: ');
-  pessoa.nome = stdin.readLineSync();
-  print('Peso: ');
-  pessoa.peso = double.parse(stdin.readLineSync()!);
+  pessoa.nome = stdin.readLineSync()!;
+  print('Idade: ');
+  pessoa.idade = int.parse(stdin.readLineSync()!);
 
   String n = 'Thalles';
-  double p = 60.0;
-  var pessoa2 = Pessoa(nome: n, peso: p);
+  int i = 60;
+  var pessoa2 = Pessoa(nome: n, idade: i);
 
-  print('Nome: ${pessoa.nome} Peso: ${pessoa.peso}');
+  print('Nome: ${pessoa.nome} Peso: ${pessoa.idade}');
 
   Carro carro =
       Carro(nome: 'Ferrari', peso: 1000.0, altura: 1.50, imc: calcularIMC);
